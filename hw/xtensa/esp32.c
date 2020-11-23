be11806e1c17b29dbe8e4c15e3c67f06b7707771
Max Sydney <maxsydney8@gmail.com>
2020-11-23 22:30:35 +1300

    Added line to ignore ledc driver missing. Added gitignore

--- hw/xtensa/esp32.c
+++ hw/xtensa/esp32.c
@@ -450,7 +450,7 @@ static void esp32_soc_realize(DeviceState *dev, Error **errp)
     esp32_soc_add_unimp_device(sys_mem, "esp32.i2s1", DR_REG_I2S1_BASE, 0x1000);
     esp32_soc_add_unimp_device(sys_mem, "esp32.i2c0", DR_REG_I2C_EXT_BASE, 0x1000);
     esp32_soc_add_unimp_device(sys_mem, "esp32.i2c1", DR_REG_I2C1_EXT_BASE, 0x1000);
-
+    esp32_soc_add_unimp_device(sys_mem, "esp32.ledc", DR_REG_LEDC_BASE, 0x1000);
     qemu_register_reset((QEMUResetHandler*) esp32_soc_reset, dev);
 }
 
